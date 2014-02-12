d7e08a846d9074999903be0b8044a3532f9974cf
SmBe19 <bsgame27@gmail.com>
2014-02-12 15:33:44 +0100

    Changed README; Added mainclass

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 EFRisiko
 ========
+
+Abschlussprojekt für das EF-Informatik an der KSO.
+
+(c) 2014 Benjamin Schmid
---
+++ src/EFRisiko/efRisiko.java
@@ -0,0 +1,24 @@
+/**
+ * efRisiko.java
+ * (c) 2014 Benjamin Schmid
+ * Created: 12.02.2014
+ * 
+ * Main class for the simulation.
+ * Initializes the GameCore and starts the GameInterface.
+ */
+
+package EFRisiko;
+
+public class efRisiko {
+
+	public static void main(String[] args) {
+		System.out.println("EFRisiko\n(c) 2014 Benjamin Schmid");
+		System.out.println("Initialize GameCore...");
+		// ToDo: Init GameCore
+		System.out.println("Done!");
+		System.out.println("Initialize GameInterface...");
+		// ToDo: Init GameInterface
+		System.out.println("Done!");
+	}
+
+}
